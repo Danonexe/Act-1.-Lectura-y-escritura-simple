50e106093f64553a8ecfadcca5d18aab73a3cd83
Danonexe <danielhgomez2005@gmail.com>
2024-09-26 21:16:46 +0200

    Update EmpresaMap.kt

--- Act1_LecturaEscritura/src/main/kotlin/Act1_Lectura_escritura_simple/EmpresaMap.kt
+++ Act1_LecturaEscritura/src/main/kotlin/Act1_Lectura_escritura_simple/EmpresaMap.kt
@@ -3,15 +3,14 @@ package Act1_Lectura_escritura_simple
 import java.nio.file.Files
 import java.nio.file.Path
 import java.io.BufferedReader
-import java.io.IOException
 
-//leer los datos y guardarlos en un map
+// Leer los datos y guardarlos en un map
 fun leerCotizaciones(ficheroArchivo: Path): Map<String, MutableList<Double>> {
     val datosPorColumna = mutableMapOf<String, MutableList<Double>>()
 
     BufferedReader(Files.newBufferedReader(ficheroArchivo)).use { br ->
-        //Datos de arriba la excepción es por si el archivo está vacio
-        val cabecera = br.readLine()?.split(";") ?: throw IOException("Archivo vacío")
+
+        val cabecera = br.readLine()!!.split(";")
 
         // Inicializar las columnas
         cabecera.forEach { columna ->
@@ -23,21 +22,23 @@ fun leerCotizaciones(ficheroArchivo: Path): Map<String, MutableList<Double>> {
             val valores = linea.split(";")
             for (i in 1 until valores.size) {
                 val columna = cabecera[i]
-                //esto me dio muchos errores
-                val valor = valores[i].replace(".", "").replace(",", ".")//Cambiar comas
-                    datosPorColumna[columna]?.add(valor.toDouble())
+                // Conversión de valores
+                val valor = valores[i].replace(".", "").replace(",", ".")
+                datosPorColumna[columna]?.add(valor.toDouble())
             }
         }
     }
+
     return datosPorColumna
 }
-//estadisticas
+
+// Estadísticas
 fun calcularEstadisticas(datos: Map<String, List<Double>>, directorioNuevo: Path) {
     val estadisticas = mutableListOf("Columna;Minimo;Maximo;Media")
 
     datos.forEach { (columna, valores) ->
         if (valores.isNotEmpty()) {
-            // la media el minimo y el máximo lo he buscado en internet
+            // Calcular estadísticas
             val min = valores.minOrNull() ?: 0.0
             val max = valores.maxOrNull() ?: 0.0
             val media = valores.average()
